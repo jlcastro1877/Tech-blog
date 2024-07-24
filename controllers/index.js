39199222d1a851d51f2d7c4bab15a58f7fe0ee58
jlcastro1877 <jlcastro1877@gmail.com>
2024-07-23 20:39:13 -0600

    index.js routes done

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+// import the necessary modules and routes
+const router = require("express").Router;
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
+// Set up routes
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
