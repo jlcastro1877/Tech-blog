410872e2256801825a194bb6628fdf1eab4b6fdc
jlcastro1877 <jlcastro1877@gmail.com>
2024-07-23 19:57:19 -0600

    installed connect-session-sequelize

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+require("dotenv").config(); //Import sensitive data from .env
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "postgres",
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  }
+);
+
+module.exports = sequelize;
