cfc7c2203426efb67557fef17d047029c93a575c
jlcastro1877 <jlcastro1877@gmail.com>
2024-07-26 01:10:53 -0600

    fixed redirect login

--- utils/auth.js
+++ utils/auth.js
@@ -2,7 +2,8 @@
 const withAuth = (req, res, next) => {
   // If user is not logged in, redirect to login page
   if (!req.session.logged_in) {
-    res.redirect("/login");
+    // res.redirect("/login");
+    res.redirect("/dashboard");
   } else {
     // If user is logged in, continue to next middleware or route
     next();
