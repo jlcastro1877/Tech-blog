e293b6af28c38dec178736bded21ed16889eaac1
jlcastro1877 <jlcastro1877@gmail.com>
2024-07-25 23:02:36 -0600

    fixed .env cleaning up connection

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,11 @@ const Sequelize = require("sequelize");
 require("dotenv").config(); //Import sensitive data from .env
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASSWORD,
+  process.env.DB_URL,
+
   {
     host: "localhost",
     dialect: "postgres",
