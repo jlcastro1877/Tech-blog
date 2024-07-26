a182e4bbdb02f1af030c07617b97834076756e9f
jlcastro1877 <jlcastro1877@gmail.com>
2024-07-26 00:01:19 -0600

    added variables of .env

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,24 @@
+// require("dotenv").config(); //Import sensitive data from .env
+// const Sequelize = require("sequelize");
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   process.env.DB_URL,
+//   process.env.DB_PORT,
+
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     dialectOptions: {
+//       decimalNumbers: true,
+//     },
+//   }
+// );
+
+// module.exports = sequelize;
+
 require("dotenv").config(); // Carrega variáveis de ambiente
 const { Sequelize } = require("sequelize");
 
@@ -11,19 +32,27 @@ console.log("DB_URL:", databaseUrl); // Para depuração
 
 const sequelize = new Sequelize(databaseUrl, {
   dialect: "postgres",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  pool: {
+    max: 5, // Ajuste conforme necessário
+    min: 0,
+    acquire: 30000, // Tempo máximo de aquisição de conexão em ms
+    idle: 10000, // Tempo máximo de inatividade da conexão em ms
+  },
   dialectOptions: {
     decimalNumbers: true,
     ssl: {
       require: true,
       rejectUnauthorized: false, // Ajuste conforme necessário
     },
+    keepAlive: true,
   },
-  pool: {
-    max: 10, // Ajuste conforme necessário
-    min: 0,
-    acquire: 30000, // Tempo máximo de aquisição de conexão em ms
-    idle: 10000, // Tempo máximo de inatividade da conexão em ms
-  },
+  ssl: true,
+
   logging: false, // Desativa o log SQL
 });
 
