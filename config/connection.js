7be25102568d2ce461008cb221209a74862acf5b
jlcastro1877 <jlcastro1877@gmail.com>
2024-07-26 00:47:25 -0600

    redeploy

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,34 @@
+require("dotenv").config(); // Carrega variáveis de ambiente
+const { Sequelize } = require("sequelize");
+
+const databaseUrl = process.env.DB_URL;
+
+if (!databaseUrl) {
+  throw new Error("A variável de ambiente DB_URL não está definida.");
+}
+
+console.log("DB_URL:", databaseUrl); // Para depuração
+
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: "postgres",
+  dialectOptions: {
+    decimalNumbers: true,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Ajuste conforme necessário
+    },
+  },
+  pool: {
+    max: 10, // Ajuste conforme necessário
+    min: 0,
+    acquire: 30000, // Tempo máximo de aquisição de conexão em ms
+    idle: 10000, // Tempo máximo de inatividade da conexão em ms
+  },
+  logging: false, // Desativa o log SQL
+});
+
+module.exports = sequelize;
+
 // require("dotenv").config(); //Import sensitive data from .env
 // const Sequelize = require("sequelize");
 
@@ -19,41 +50,41 @@
 
 // module.exports = sequelize;
 
-require("dotenv").config(); // Carrega variáveis de ambiente
-const { Sequelize } = require("sequelize");
+// // require("dotenv").config(); // Carrega variáveis de ambiente
+// // const { Sequelize } = require("sequelize");
 
-const databaseUrl = process.env.DB_URL;
+// // const databaseUrl = process.env.DB_URL;
 
-if (!databaseUrl) {
-  throw new Error("A variável de ambiente DB_URL não está definida.");
-}
+// // if (!databaseUrl) {
+// //   throw new Error("A variável de ambiente DB_URL não está definida.");
+// // }
 
-console.log("DB_URL:", databaseUrl); // Para depuração
+// // console.log("DB_URL:", databaseUrl); // Para depuração
 
-const sequelize = new Sequelize(databaseUrl, {
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  pool: {
-    max: 5, // Ajuste conforme necessário
-    min: 0,
-    acquire: 30000, // Tempo máximo de aquisição de conexão em ms
-    idle: 10000, // Tempo máximo de inatividade da conexão em ms
-  },
-  dialectOptions: {
-    decimalNumbers: true,
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // Ajuste conforme necessário
-    },
-    keepAlive: true,
-  },
-  ssl: true,
+// // const sequelize = new Sequelize(databaseUrl, {
+// //   dialect: "postgres",
+// //   host: process.env.DB_HOST,
+// //   port: process.env.DB_PORT,
+// //   database: process.env.DB_NAME,
+// //   username: process.env.DB_USER,
+// //   password: process.env.DB_PASSWORD,
+// //   pool: {
+// //     max: 5, // Ajuste conforme necessário
+// //     min: 0,
+// //     acquire: 30000, // Tempo máximo de aquisição de conexão em ms
+// //     idle: 10000, // Tempo máximo de inatividade da conexão em ms
+// //   },
+// //   dialectOptions: {
+// //     decimalNumbers: true,
+// //     ssl: {
+// //       require: true,
+// //       rejectUnauthorized: false, // Ajuste conforme necessário
+// //     },
+// //     keepAlive: true,
+// //   },
+// //   ssl: true,
 
-  logging: false, // Desativa o log SQL
-});
+// //   logging: false, // Desativa o log SQL
+// // });
 
-module.exports = sequelize;
+// // module.exports = sequelize;
