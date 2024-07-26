5427a26296653becf91fda9b16d17e764f0110fa
jlcastro1877 <jlcastro1877@gmail.com>
2024-07-25 23:26:58 -0600

    added ssl

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,26 @@
-require("dotenv").config();
-const Sequelize = require("sequelize");
+require("dotenv").config(); // Carrega variáveis de ambiente
+const { Sequelize } = require("sequelize");
 
+// Verifique se a variável de ambiente está definida
 const databaseUrl = process.env.DB_URL;
-
-console.log("DB_URL:", databaseUrl); // Adicione esta linha para depuração
 
 if (!databaseUrl) {
   throw new Error("A variável de ambiente DB_URL não está definida.");
 }
 
+console.log("DB_URL:", databaseUrl); // Para depuração
+
+// Inicialize o Sequelize com a URL do banco de dados e opções de SSL
 const sequelize = new Sequelize(databaseUrl, {
   dialect: "postgres",
   dialectOptions: {
     decimalNumbers: true,
+    ssl: {
+      require: true, // Requer SSL
+      rejectUnauthorized: false, // Opcional: Pode ser necessário dependendo da configuração do banco
+    },
   },
-  logging: false,
+  logging: false, // Opcional: desativa o log SQL
 });
 
 module.exports = sequelize;
