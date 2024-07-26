171f6844fe90729f11c063c6ead5a9f36ce8aecf
jlcastro1877 <jlcastro1877@gmail.com>
2024-07-25 23:14:58 -0600

    adjusting require(dotenv)

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,20 @@
+require("dotenv").config();
 const Sequelize = require("sequelize");
-require("dotenv").config(); //Import sensitive data from .env
 
-const sequelize = new Sequelize(
-  // process.env.DB_NAME,
-  // process.env.DB_USER,
-  // process.env.DB_PASSWORD,
-  process.env.DB_URL,
+const databaseUrl = process.env.DB_URL;
 
-  {
-    host: "localhost",
-    dialect: "postgres",
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-  }
-);
+console.log("DB_URL:", databaseUrl); // Adicione esta linha para depuração
+
+if (!databaseUrl) {
+  throw new Error("A variável de ambiente DB_URL não está definida.");
+}
+
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: "postgres",
+  dialectOptions: {
+    decimalNumbers: true,
+  },
+  logging: false,
+});
 
 module.exports = sequelize;
